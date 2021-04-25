9e762086401fcae1526188dec80f80b8b1c5d73a
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2021-04-26 00:12:46 +0530

    Project completed.

---
+++ SayIt/assets/js/comment.js
@@ -0,0 +1,14 @@
+$(function()
+{
+	$('#postComment').click(function()
+	{
+		var comment = $('#commentField').val();
+		var tweet_id = $('#commentField').data('tweet');
+
+		$.post('http://localhost/php projects/SayIt/core/ajax/comment.php', {comment:comment,tweet_id:tweet_id}, function(data)
+		{
+			$('#comments').html(data);
+			$('#commentField').val('');
+		});
+	});
+});
